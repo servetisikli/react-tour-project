3c81166230985661da4797cb621b9f59ae0df66f
servetisikli <servetisikli@gmail.com>
2023-05-24 10:52:40 +0200

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="App">
+      <Navbar/>
     </div>
   );
 }
