e08faaf2cbd05187234401d7996ce8a01bc62c29
servetisikli <servetisikli@gmail.com>
2023-05-24 20:35:30 +0200

    all components added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
+import Cards from "./components/cards/Cards";
 
 export default function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Header/>
+      <Navbar />
+      <Header />
+      <Cards />
     </div>
   );
 }
--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,11 +1,27 @@
+import React from "react";
 import "./Cards.css";
+import { data } from "../../helpers/data";
 
-import React from 'react'
+console.log(data);
 
 const Cards = () => {
   return (
-    <div>Cards</div>
-  )
-}
+    <div className="card-container">
+      {data.map((card, index) => {
+        return (
+          <div className="cards" key={index}>
+            <div className="title">
+              <h2>{card.title}</h2>
+            </div>
+            <img src={card.image} alt={card.title} />
+            <div className="card-over">
+              <p>{card.desc}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Cards
+export default Cards;
